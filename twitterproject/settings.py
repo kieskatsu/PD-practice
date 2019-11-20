2a32e07ab80160efe54cc4b33276c6599ba46c67
kieskatsu <b1814660@planet.kanazawa-it.ac.jp>
2019-11-20 12:26:13 +0000

    first publish

--- twitterproject/settings.py
+++ twitterproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+$!t$t069aq^%vel#v2jg20%@0jgnmd2$n^nraptuhq+#o9d%('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.179.112.142', 'localhost']
 
 
 # Application definition
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'katsu',
+        'PASSWORD': 'Nl.gCn-B',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
