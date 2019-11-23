f235989acbbcb55705d23a28f19f96c5709bbc36
root <root@LAPTOP-5GMBSA99.localdomain>
2019-11-23 23:30:53 +0900

    third

--- tweet-data-collect.py
+++ tweet-data-collect.py
@@ -63,7 +63,7 @@ def save_tweets_data(list):
         traceback.print_exc()
 
 def main():
-    tweets_list = get_search_info(search=u'#かわいい動物', count=2)
+    tweets_list = get_search_info(search=u'#かわいい動物', count=1)
     save_tweets_data(tweets_list)
 
 if __name__ == '__main__':
--- twitterproject/settings.py
+++ twitterproject/settings.py
@@ -125,6 +125,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 MEDIA_URL = '/media/'
