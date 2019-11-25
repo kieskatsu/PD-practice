665abd0583fe4028ca9215166a2c873bd3f168d2
kieskatsu <b1814660@planet.kanazawa-it.ac.jp>
2019-11-25 15:53:47 +0000

    fourth

--- tweet-data-collect.py
+++ tweet-data-collect.py
@@ -1,6 +1,9 @@
-# coding:utf-8
+#!usr/bin/env/python
+# coding: utf-8
+import time
 import sqlite3
 import twitter
+import sys
 
 access_token_key = '2756721354-lyn1tMpWGfBTC2gXBNY2gq2dCrTbSe5yAW4kki6'
 access_token_secret = 'g5f8bu2oAdyTd6b8JzNyHAIoYOKfqgpAyNXYVFZiayJA9'
@@ -28,7 +31,7 @@ def get_search_info(search, count):
     return [[(name, screen_name, 0),...], [(embed_code, screen_name, tweet_id),...]]
     '''
     try:
-        tweets = api.GetSearch(term=search, count=count, lang="ja", result_type="recent")
+        tweets = api.GetSearch(term=search, count=count, lang="ja", result_type="mixed")
         user_list = []
         tweet_list =[]
         for tweet in tweets:
@@ -47,24 +50,30 @@ def save_tweets_data(list):
         con = sqlite3.connect('./db.sqlite3')
         cur = con.cursor()
 
-        extinct_sql = "select screen_name from twitterapp_twitteruserdata"
-        screen_name_list = [i for i in cur.execute(extinct_sql)] # make extinct user_list
-        user_list = set(filter(lambda x: x[1] not in screen_name_list, list[0]))
+        extinct_user_sql = u"select name, screen_name from twitterapp_twitteruserdata"
+        extinct_tweet_sql = u"select tweet_id from twitterapp_tweetdata"
+        screen_name_list = [(i[0], i[1]) for i in cur.execute(extinct_user_sql)] # make extinct user_list
+        user_list = set(filter(lambda x:(x[0], x[1]) not in screen_name_list, list[0]))
+        tweet_id_list = [j[0] for j in cur.execute(extinct_tweet_sql)] # make extinct tweet_list
+        tweet_list = set(filter(lambda x: x[2] not in tweet_id_list, list[1]))
 
         user_sql = u"insert into twitterapp_twitteruserdata (name, screen_name, point) values (?,?,?)"
         tweet_sql = u"insert into twitterapp_tweetdata (embed_code, screen_name_id, tweet_id) values(?,?,?)"
 
-        cur.executemany(user_sql, user_list) #user_list (converted)
-        cur.executemany(tweet_sql, list[1]) #tweet_list
-        con.commit()
-        con.close()
+        if user_list:
+            cur.executemany(user_sql, user_list) #user_list (converted)
+            cur.executemany(tweet_sql, tweet_list) #tweet_list (converted)
+            con.commit()
+            con.close()
     except:
         import traceback
         traceback.print_exc()
 
 def main():
-    tweets_list = get_search_info(search=u'#かわいい動物', count=2)
-    save_tweets_data(tweets_list)
+        tweets_list = get_search_info(search=u'#かわいい動物', count=10)
+        save_tweets_data(tweets_list)
 
 if __name__ == '__main__':
-    main()
+    while 1:
+        main()
+        time.sleep(20)
